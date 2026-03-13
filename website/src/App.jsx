d7bf35b71185f38c27466d85d03c78a435aca6e9
Pratyakshgupta887qwert <pratyakshgupta887@gmail.com>
2026-03-13 19:45:05 +0530

    website setup

---
+++ website/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-500">
+  Tailwind Working 🚀
+</h1>
+    </>
+  )
+}
+
+export default App
